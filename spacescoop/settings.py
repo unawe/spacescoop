100ec4723834db1ecbbc5fafc4dc02b907e5aee0
Edward Gomez <edward@gomez.me.uk>
2020-09-24 11:22:11 +0100

    Setting up db

--- spacescoop/settings.py
+++ spacescoop/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -73,12 +74,8 @@ WSGI_APPLICATION = 'spacescoop.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+DEFAULT_DATABASE_DSN = os.environ.get('DEFAULT_DATABASE_DSN')
+DATABASES = {'default': dj_database_url.parse(DEFAULT_DATABASE_DSN)}
 
 
 # Password validation
