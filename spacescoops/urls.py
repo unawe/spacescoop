877ad7c9b984469d83ccf1abe122225c7456f5fa
Edward Gomez <edward@gomez.me.uk>
2020-10-14 14:01:26 +0100

    Remove PDF generation url space

--- spacescoops/urls.py
+++ spacescoops/urls.py
@@ -12,7 +12,7 @@ urlpatterns = [
     url(r'^$', views.ArticleListView.as_view(), name='list'),
     url(r'^feed/$', views.ArticleFeed(), name='feed'),
     url(r'^(?P<code>\d{4})/$', views.detail_by_code),
-    url(r'^(?P<code>\d{4})/pdf/$', views.ArticlePDFView.as_view(), name='pdf'),
+    # url(r'^(?P<code>\d{4})/pdf/$', views.ArticlePDFView.as_view(), name='pdf'),
     url(r'^(?P<code>\d{4})/(?P<slug>.+)?/$', views.ArticleDetailView.as_view(), name='detail'),
     url(r'^institution/(?P<institution>.+)/$', views.ArticleListView.as_view(), name='list_by_institution'),
     re_path(r'^(?P<url>.*/)$', SmartPageView.as_view(), name='smartpage'),
